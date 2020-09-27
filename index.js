d73b13fe8918eda6825cfceabe690b6a0af47eeb
Hajar Helal <hajerhelal44@gmail.com>
2020-09-27 01:19:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ function testFunc() {
   return "hi"
 }
 function  superbowlWin(record){
-
+  const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+  ]
+  record.find( r => r === ""W" )
 }
